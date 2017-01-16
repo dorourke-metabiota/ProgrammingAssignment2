f393e168b3f06b12da90d79be32fd5473a3293d6
Daniel <dorourke@metabiota.com>
2017-01-16 13:26:40 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ m <- NULL
+        
+        StoreMatrix <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        getMatrix <- function() x
+        
+        setsolve <- function(solve) {
+                m <<- solve
+        }
+        
+        getsolve <- function ()
+                m
+                
+        
+        
+        list(StoreMatrix = StoreMatrix, getMatrix = getMatrix, setsolve = setsolve, getsolve = getsolve)
+        m
 }
 
 
@@ -12,4 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         m <- x$getsolve()
+        if (!is.null(m)) {
+                message ("getting cached data")
+                return(m)
+        }
+        data <- x$getMatrix()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
